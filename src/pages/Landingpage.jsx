cde6750fa30c9f803d716aeb9489aa324da9603c
kennedybabu <kennedynjogu8@gmail.com>
2022-11-17 22:00:14 +0300

    feature: style the landingpage

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Landingpage = () => {
   return (
     <div className='w-full h-screen landing relative z-10'>
-        <div className="overlay z-20 min-h-screen min-w-full">
-            
+        <div className="overlay z-20 min-h-screen min-w-full flex items-center text-white justify-center flex-col">
+            <small className=''>welcome to</small>
+            <h1 className='text-3xl font-[600]'>Grand <span className='text-[#df9a57]'>Hotel</span></h1>
+            <p className='tracking-wide text-[15px]'>A place to experience and enjoy the life</p>
         </div>
     </div>
   )
