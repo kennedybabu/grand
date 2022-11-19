d65ea00f503148e7fb43d811f1efbabeb9a38d2d
kennedybabu <kennedynjogu8@gmail.com>
2022-11-19 11:31:17 +0300

    feature: use NavLink instead of Link in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import {GiShintoShrine} from 'react-icons/gi'
 import {RiMenu3Fill} from 'react-icons/ri'
 import {MdClose} from 'react-icons/md'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     const [nav,setNav] = useState(false)
@@ -12,22 +12,30 @@ const Navbar = () => {
     }
 
 
+    let activeStyle = {
+        textDecoration :"underline",
+        color:'#df9a57'
+    }
+
+    let activeClassName = "underline"
+
+
   return (
-    <div className='w-full h-[100px] absolute left-0 top-0 px-4 flex items-center justify-between z-50 text-white'>
-        <div className='h-full flex flex-col items-center justify-center'>
+    <div className='w-full h-[100px] absolute left-0 top-0 px-4 flex items-center justify-between z-50 text-white md:px-8'>
+        <div className='h-full flex flex-col items-center justify-center md:text-xl'>
             <GiShintoShrine className='text-[#df9a57]'/>
             <h2 className='brand-name'>Grand</h2>
             <small className='text-[8px] uppercase'>Hotel and Restaurant</small>
         </div>
         <div>
-            <ol className='hidden'>
-                <Link to='/'>Home</Link>
-                <Link to='accomodation'>Accomodation</Link>
-                <li>Gallery</li>
-                <li>News</li>
-                <li>Shop</li>exact
-                <li>Contact</li>
-            </ol>
+            <div className='hidden md:flex md:text-[13px]'>
+                <NavLink style={({isActive}) => isActive ? activeStyle : undefined} className='mx-1 hover:border-t-2 transition' to='/'>Home</NavLink>
+                <NavLink style={({isActive}) => isActive ? activeStyle : undefined} className='mx-1 hover:border-t-2 transition' to='accomodation'>Accomodation</NavLink>
+                <NavLink className='mx-1 hover:border-t-2 transition' >Gallery</NavLink>
+                <NavLink className='mx-1 hover:border-t-2 transition' >News</NavLink>
+                <NavLink className='mx-1 hover:border-t-2 transition' >Shop</NavLink>
+                <NavLink className='mx-1 hover:border-t-2 transition' >Contact</NavLink>
+            </div>
         </div>
         <div className='md:hidden z-40' onClick={(e) => setNav(!nav)}>            
             {nav ? <MdClose className='text-[#202020] fixed right-4 top-12'/> : <RiMenu3Fill />}
@@ -35,12 +43,12 @@ const Navbar = () => {
         {nav ? (
             <div id='mobile-nav' className='fixed right-0 w-[70%] text-[#202020] top-0 bottom-0 bg-[#ffffff] min-h-screen flex items-center justify-center flex-col'>
                 <li className='flex flex-col justify-center'>
-                    <Link to='/' onClick={toggleNav}>Home</Link>
-                    <Link to='accomodation' onClick={toggleNav}>Accomodation</Link>
-                    <Link onClick={toggleNav}>Gallery</Link>
-                    <Link onClick={toggleNav}>News</Link>
-                    <Link onClick={toggleNav}>Shop</Link>
-                    <Link onClick={toggleNav}>Contact</Link>
+                    <NavLink to='/' onClick={toggleNav}>Home</NavLink>
+                    <NavLink to='accomodation' onClick={toggleNav}>Accomodation</NavLink>
+                    <NavLink onClick={toggleNav}>Gallery</NavLink>
+                    <NavLink onClick={toggleNav}>News</NavLink>
+                    <NavLink onClick={toggleNav}>Shop</NavLink>
+                    <NavLink onClick={toggleNav}>Contact</NavLink>
                 </li>
             </div>
         ): ( null )}
