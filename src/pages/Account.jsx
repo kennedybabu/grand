2bb33dee4442fbf9bca1a452b8615c06eb6c5d69
kennedybabu <kennedynjogu8@gmail.com>
2022-11-26 11:09:32 +0300

    feature:change the room ids

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import Header from '../components/Header'
+import data from '../data/data.json'
+
 
 
 const Account = () => {
   const {id} = useParams()
+
+
   return (
     <div className='w-full h-screen'>
         <Header text={'Account'} />
         <div className='w-full  flex flex-col items-center justify-center'>
             <p>trials and errors</p>
-            {id ? <p>{id}</p> : null}
         </div>
     </div>
   )
